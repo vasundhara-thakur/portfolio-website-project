4e001cbf6e73fe2a5e42423bb4d5782cec870bf6
vasundhara thakur <vasundharathakur01@gmail.com>
2025-05-12 20:19:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("contact-form").addEventListener("submit", function (e) {
   e.preventDefault();
-  alert("Thank you! Your message has been sent.");
+  alert("Your message can not be sent. You can connect through email or linkedin.Thank you!");
   this.reset();
 });
 
